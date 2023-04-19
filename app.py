72c89682970e1c55da018e6e568b99979672d20e
SebastianSosa <s.sosa@live.fr>
2023-04-19 15:55:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -101,14 +101,14 @@ index = np.where(data.SK_ID_CURR == clientsIDS)
 backup = data
 ## Process data for ML ------------------------------------
 
-r = requests.get("https://mlflowbank.drsosa.repl.co/get/" + str(index)).json()
+#r = requests.get("https://mlflowbank.drsosa.repl.co/get/" + str(index)).json()
 data = processData(data)
 model.predict_proba(data[clientsIDS == id])
 
 idLOC = np.where(clientsIDS == id)
 st.markdown("# Predicted probabilities:")
 st.write(model.predict_proba(np.array(data.iloc[idLOC[0],:])))
-
+st.write(index)
 #############################################
 ## PLots
 #############################################
