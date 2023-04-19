8e11f2e511dea46e42032683ec97ea9bb4c08a8d
SebastianSosa <s.sosa@live.fr>
2023-04-19 15:58:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ model.predict_proba(data[clientsIDS == id])
 idLOC = np.where(clientsIDS == id)
 st.markdown("# Predicted probabilities:")
 st.write(model.predict_proba(np.array(data.iloc[idLOC[0],:])))
-st.write(index[0])
+st.write(index[0][0])
 #############################################
 ## PLots
 #############################################
